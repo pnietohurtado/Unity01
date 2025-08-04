c33f91b39d1655bc53b798f23545cbcac7e8a43d
pnh0002 <pnh0002@alu.medac.es>
2025-08-04 13:30:30 +0200

    Terminamos la primera clase sobre Unity

--- Unity01/Assets/Script/Player.cs
+++ Unity01/Assets/Script/Player.cs
@@ -10,14 +10,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(new Vector3(-0.1f, 0.0f)); 
         }
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(new Vector3(0.1f, 0.0f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // transform.Translate(new Vector3(0.0f, 0.1f)); 
+
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 500.0f));
         }
 
     }
